86d973d60128f4390275ded488c21fa723e1b22e
Kaka <vn503024@gmail.com>
2024-09-24 15:51:21 +0800

    feat: added the helper class for operate Google Sheet by pygsheets

---
+++ config/prod.py
@@ -0,0 +1,6 @@
+import os
+
+config = {
+    "google_sheet_key": os.getenv("GOOGLE_SHEET_KEY"),
+    "google_sheet_url": os.getenv("GOOGLE_SHEET_URL"),
+}
---
+++ google_sheet.py
@@ -0,0 +1,45 @@
+import pygsheets
+import pandas as pd
+
+from config.dev import config
+
+
+class google_sheet_helper:
+    def __init__(self, service_file, url):
+        # authorization
+        self.gc = pygsheets.authorize(service_file=service_file)
+        self.sht = self.gc.open_by_url(url)
+        
+        # 讀取全部工作表
+        self.wks_list = self.sht.worksheets()
+    
+    def select_worksheet_by_index(self, index=0):
+        # 選取工作表 by index
+        self.wks = self.sht[index]
+    
+    def select_worksheet_by_title(self, title):
+        # 指定工作表 by 名稱
+        self.wks = self.sht.worksheet_by_title(title)
+
+    def read_cell(self, position="A1"):
+        return self.wks.cell(position).value
+    
+    def read_all(self, start='A1', index_colum=0, empty_value='', include_tailing_empty=False):
+        # pd.DataFrame(wks.get_all_records())
+        return self.wks.get_as_df(start=start, index_colum=index_colum, empty_value=empty_value, include_tailing_empty=include_tailing_empty)
+
+    def export(self, file_format=pygsheets.ExportType.CSV, filename='test', path=''):
+        self.wks.export(file_format=file_format, filename=filename, path=path)
+
+
+if __name__ == "__main__":
+    # 透過憑證連接 google sheet
+    service_file = config["google_sheet_key"]
+    url = config["google_sheet_url"]
+    gc = google_sheet_helper(service_file=service_file, url=url)
+   
+    # 選取工作表
+    gc.select_worksheet_by_index(0)
+
+    # 讀取全部資料
+    print(gc.read_all())
